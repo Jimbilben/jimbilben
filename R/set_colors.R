3b10e8986ab0762b5c9d5425f1467c9f9962ee5b
Jimbilben <jamie@rethinkpriorities.org>
2023-08-25 16:30:18 +0100

    adding set_colors

---
+++ R/set_colors.R
@@ -0,0 +1,36 @@
+#' Set colors
+#'
+#' Set a range of colors as objects in the environment
+#'
+#' @param print Logical, whether or not to print the names of the colors that have been set
+#' @export
+
+set_colors <- function(print = TRUE) {
+
+  rp_blue <<- "#327291"
+  rp_yellow <<- "#f2b831"
+  light_yellow <<- "#efd394"
+  light_blue <<- "#98cbe4"
+
+  dark_blue <- "#184f6a"
+
+  dust_pink <<- "#ffb1a6"
+  light_dust_pink <<- "#ffcfc8"
+  dark_dust_pink <<- "#de8579"
+
+  dark_yellow <<- "#de9f10"
+
+  mid_blue <<- "#4686a4"
+
+  fire_red <<- "#ea5b21"
+  light_fire_red <<- "#ee703d"
+  dark_fire_red <<- "#b9441e"
+
+  if(print == TRUE) {
+    print("rp_blue, mid_blue, light_blue, dark_blue")
+    print("rp_yellow, dark_yellow, light_yellow")
+    print("dust_pink, light_dust_pink, dark_dust_pink")
+    print("fire_red, light_fire_red, dark_fire_red")
+  }
+
+}
