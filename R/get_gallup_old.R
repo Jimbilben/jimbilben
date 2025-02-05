b75dbcceb71ddc7ff2a77a8451cc0c8aa1c5bed4
Jimbilben <jamie@rethinkpriorities.org>
2025-02-05 14:27:57 +0000

    Updating weighting
    Changes to Gallup data availability and reporting requirements mean the weighting for partyid needed to be changed - uses a different private url and only at the granularity of quarterly.

--- R/get_gallup_old.R
+++ R/get_gallup_old.R
@@ -1,25 +1,34 @@
 #' Get Gallup
 #'
-#' Retrieve up to date US political party affiliation numbers from Gallup. You are now required to request access to quarterly party affiliation data from Gallup, and can then enter the appropriate URL for the data. Usage of the data must involve attribution to Gallup, including when this is just used to weight survey results - see the bottom of the page here: https://news.gallup.com/poll/15370/party-affiliation.aspx
+#' Retrieve up to date US political party affiliation numbers from Gallup. NOTE: This is the old version of the function that has been shelved due to Gallup changing their policies and availability of monthly data.
 #'
-#' @param moving_average Numeric input indicating how many quarters to average over. Defaults to 1.
-#' @param gallup_url The input URL needed to get to the quarterly ratings - it is necessary to contact Gallup to get the URL.
-#' @param stop_date Character string of the form 'YYYY QQ': the date at which to stop the partyid collection, if e.g., you want to weight for data from Q2 2023 then '2023 Q2'
+#' @param moving_average Numeric input indicating how many months to average over. Defaults to 3.
+#' @param custom_url An alternative input URL in the event that the standard URL changes/does not work
+#' @param stop_date Character string of the form YYYY-MM-DD: the date at which to stop the partyid collection, if e.g., you want to weight for data from December 2023 then 2023-12-01
 
 #'
 #' @export
 
-get_gallup <- function(moving_average = 1,
-                       gallup_url = "you need to request the URL from gallup",
+get_gallup <- function(moving_average = 3,
+                       custom_url = NULL,
                        stop_date = NULL) {
 
+  if(is.null(custom_url)) {
+    gallup_url <- "https://news.gallup.com/poll/15370/party-affiliation.aspx"
+  }
+  else(
+    gallup_url <- custom_url
+  )
+
+  # navigate to the page where the table is
+  # get a list of different elements on the site
   gallup_page_main <-
     httr::GET(gallup_url) %>%
     httr::content("text", encoding = "ISO-8859-1") %>%
     strsplit(., '[[:space:]]') %>%
     unlist()
 
-  print("Step 1")
+  print(glue::glue("Step 1"))
 
   # find the elements that refer to another address and refer to datawrapper
   datawrapper_locations <-
@@ -42,7 +51,7 @@ get_gallup <- function(moving_average = 1,
   # Extract the specific https part of that element call
   data_location <-
     gallup_page_with_data[which(grepl("window.location.href", gallup_page_with_data))] %>%
-    strsplit(., "\\'") %>%
+    str_split(., "\\'") %>%
     unlist()
 
   data_page_for_warning <- data_location[2]
@@ -71,8 +80,8 @@ get_gallup <- function(moving_average = 1,
   # now read the csv file - it is \t delimited
   gallup_data <-
     read.csv(file = data_location, sep = "\t") %>%
-    tibble::as_tibble() %>%
-    dplyr::rename("og_date" = X,
+    as_tibble() %>%
+    rename("og_date" = X,
            "og_Republican" = Republican,
            "og_Independent" = Independent,
            "og_Democrat" = Democrat)
@@ -83,9 +92,7 @@ get_gallup <- function(moving_average = 1,
   # and add the specified moving average
   gallup_data <-
     gallup_data[-1, ] %>%
-    tidyr::separate(og_date,
-             into = c("year", "quarter"), sep = " ", remove = FALSE) %>%
-    dplyr::mutate(Republican = RcppRoll::roll_mean(as.numeric(og_Republican), n = moving_average, align = "left", fill = NA),
+    mutate(Republican = RcppRoll::roll_mean(as.numeric(og_Republican), n = moving_average, align = "left", fill = NA),
            Independent = RcppRoll::roll_mean(as.numeric(og_Independent), n = moving_average, align = "left", fill = NA),
            Democrat = RcppRoll::roll_mean(as.numeric(og_Democrat), n = moving_average, align = "left", fill = NA),
            Independent_other = 100 - (Republican + Democrat),
@@ -95,24 +102,34 @@ get_gallup <- function(moving_average = 1,
            prop_rep_other = Republican / (Republican + Independent_other + Democrat),
            prop_ind_other = Independent_other / (Republican + Independent_other + Democrat),
            prop_dem_other = Democrat / (Republican + Independent_other + Democrat),
-           year = as.numeric(year),
-           quarter = parse_number(quarter)) %>%
-    dplyr::relocate(og_date, Republican:prop_dem_other) %>%
-    dplyr::arrange(-year, -quarter)
+           year = parse_number(og_date),
+           month = case_when(grepl("jan", tolower(og_date)) ~ "Jan",
+                             grepl("feb", tolower(og_date)) ~ "Feb",
+                             grepl("mar", tolower(og_date)) ~ "Mar",
+                             grepl("apr", tolower(og_date)) ~ "Apr",
+                             grepl("may", tolower(og_date)) ~ "May",
+                             grepl("jun", tolower(og_date)) ~ "Jun",
+                             grepl("jul", tolower(og_date)) ~ "Jul",
+                             grepl("aug", tolower(og_date)) ~ "Aug",
+                             grepl("sep", tolower(og_date)) ~ "Sep",
+                             grepl("oct", tolower(og_date)) ~ "Oct",
+                             grepl("nov", tolower(og_date)) ~ "Nov",
+                             grepl("dec", tolower(og_date)) ~ "Dec"),
+           day = 1,
+           date = lubridate::ymd(paste(year, "/", month, "/", day, sep = ""))) %>%
+    dplyr::relocate(date, Republican:prop_dem_other)
 
   # we now want to make sure we can use older dates for weighting as well
   gallup_data <-
     if(is.null(stop_date)) {
       gallup_data
-    } else {
-      stop_row <- which(gallup_data$og_date == stop_date)
-      gallup_data <-
-        gallup_data[stop_row:nrow(gallup_data), ]
     }
-
-  gallup_data <-
+  else {
     gallup_data %>%
-    tibble::rowid_to_column("order")
+      dplyr::filter(date <= lubridate::as_date(stop_date))
+  }
+
+  gallup_data$order <- nrow(gallup_data):1
 
   warning(glue::glue("Do you want to check where I got the data? I used this URL: {redirected_url}, and the csv is: {data_location}"))
 
