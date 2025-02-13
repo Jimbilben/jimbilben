a7f94875d905928592be5560099a2c87374e6d7f
Jimbilben <jamie@rethinkpriorities.org>
2025-02-13 18:58:35 +0000

    Adding psup mutli poststrat
    These remain experimental/in development

---
+++ R/poststrat_multi_psup.R
@@ -0,0 +1,232 @@
+#' Poststratify PSup MRP Predictions
+#'
+#' Perform poststratification on the posterior predictive draws from a partially supervised (PSup) MRP model across specified subgroups.
+#'
+#' @param input The object generated from \code{psup_mrp}, containing the model and posterior predictions.
+#' @param outcome_name String. The name of the PSup variable being poststratified.
+#' @param save_output Logical. If \code{TRUE}, saves the poststratification summaries to a file. Defaults to \code{save_my_poststrat}.
+#' @param return_state Logical. Whether to include the "state" subgroup in the returned output. Defaults to \code{set_state}.
+#' @param .poststrat_tibble A data frame containing the poststratification tibble. Defaults to \code{set_my_poststrat}.
+#' @param .poststrat_epred A data frame containing the poststratification posterior predictions. Defaults to \code{set_my_epred}.
+#'
+#' @return A list of poststratification summaries, including:
+#'   \itemize{
+#'     \item \code{"population"}: The population-level summary.
+#'     \item Subgroup summaries for each variable in \code{subgroups}.
+#'   }
+#'   If \code{return_state = FALSE}, the "state" subgroup summary is excluded.
+#'
+#' @details
+#' This function takes the posterior epred draws from \code{psup_mrp} and performs poststratification
+#' at both the population level and across specified subgroups. It applies the function
+#' \code{mrp_party_psup_poststrat} for stratification and formats the output with readable labels.
+#'
+#' The function maps numeric PSup values (0, 0.5, 1) to categories, using 0.5 as the reference level.
+#' Subgroups can be arranged in a logical order (e.g., for \code{"education_collapse"} or \code{"income_ces"}).
+#' The output can optionally be saved as an RDS file for future use.
+#'
+#' @note Requires the \code{brms} and \code{cmdstanr} packages.
+#' Ensure that the \code{.poststrat_tibble} and \code{.poststrat_epred} arguments are properly formatted.
+#'
+#' @export
+poststrat_multi_psup <- function(input,
+                                 outcome_name,
+                                 save_output = save_my_poststrat,
+                                 return_state = set_state,
+                                 .poststrat_tibble = set_my_poststrat,
+                                 .poststrat_epred = set_my_epred) {
+
+  # overall population level
+  print(glue::glue("getting population level for {outcome_name}"))
+  population_summary <-
+    mrp_party_psup_poststrat(input$epred,
+                             superior_name = "1",
+                             inferior_name = "0",
+                             equal_name = "0.5",
+                             comparison = outcome_name,
+                             poststrat_tibble = .poststrat_tibble,
+                             poststrat_epred = .poststrat_epred)
+
+  population_summary$summary <-
+    population_summary$summary %>%
+    mutate(new_label = glue::glue("**{nice_num(mean, 2, TRUE)}** [{nice_num(lower, 2, TRUE)}; {nice_num(upper, 2, TRUE)}]")) %>%
+    relocate(comparison, grouping_type, new_label)
+
+  # subgroups
+  ## education_collapse
+  print(glue::glue("stratifying by education_collapse for {outcome_name}"))
+  education_collapse_summary <-
+    mrp_party_psup_poststrat(input$epred,
+                             superior_name = "1",
+                             inferior_name = "0",
+                             equal_name = "0.5",
+                             comparison = outcome_name,
+                             subgroups = TRUE,
+                             poststrat_tibble = .poststrat_tibble %>% group_by(education_collapse),
+                             poststrat_epred = .poststrat_epred)
+
+  education_collapse_summary$summary <-
+    education_collapse_summary$summary %>%
+    mutate(new_label = glue::glue("**{nice_num(mean, 2, TRUE)}** [{nice_num(lower, 2, TRUE)}; {nice_num(upper, 2, TRUE)}]"),
+           grouping_type = "Education") %>%
+    relocate(comparison, grouping_type, new_label) %>%
+    arrange(education_collapse)
+
+  ## race
+  print(glue::glue("stratifying by race for {outcome_name}"))
+  race_summary <-
+    mrp_party_psup_poststrat(input$epred,
+                             superior_name = "1",
+                             inferior_name = "0",
+                             equal_name = "0.5",
+                             comparison = outcome_name,
+                             subgroups = TRUE,
+                             poststrat_tibble = .poststrat_tibble %>% group_by(race),
+                             poststrat_epred = .poststrat_epred)
+
+  race_summary$summary <-
+    race_summary$summary %>%
+    mutate(new_label = glue::glue("**{nice_num(mean, 2, TRUE)}** [{nice_num(lower, 2, TRUE)}; {nice_num(upper, 2, TRUE)}]"),
+           grouping_type = "Race") %>%
+    relocate(comparison, grouping_type, new_label)
+
+  ## income_ces
+  print(glue::glue("stratifying by income_ces for {outcome_name}"))
+  income_ces_summary <-
+    mrp_party_psup_poststrat(input$epred,
+                             superior_name = "1",
+                             inferior_name = "0",
+                             equal_name = "0.5",
+                             comparison = outcome_name,
+                             subgroups = TRUE,
+                             poststrat_tibble = .poststrat_tibble %>% group_by(income_ces),
+                             poststrat_epred = .poststrat_epred)
+
+  income_ces_summary$summary <-
+    income_ces_summary$summary %>%
+    mutate(new_label = glue::glue("**{nice_num(mean, 2, TRUE)}** [{nice_num(lower, 2, TRUE)}; {nice_num(upper, 2, TRUE)}]"),
+           grouping_type = "Income") %>%
+    relocate(comparison, grouping_type, new_label) %>%
+    arrange(income_ces)
+
+  ## male
+  print(glue::glue("stratifying by male for {outcome_name}"))
+  male_summary <-
+    mrp_party_psup_poststrat(input$epred,
+                             superior_name = "1",
+                             inferior_name = "0",
+                             equal_name = "0.5",
+                             comparison = outcome_name,
+                             subgroups = TRUE,
+                             poststrat_tibble = .poststrat_tibble %>% group_by(male),
+                             poststrat_epred = .poststrat_epred)
+
+  male_summary$summary <-
+    male_summary$summary %>%
+    mutate(new_label = glue::glue("**{nice_num(mean, 2, TRUE)}** [{nice_num(lower, 2, TRUE)}; {nice_num(upper, 2, TRUE)}]"),
+           grouping_type = "Sex") %>%
+    relocate(comparison, grouping_type, new_label)
+
+  ## partyid
+  print(glue::glue("stratifying by partyid for {outcome_name}"))
+  partyid_summary <-
+    mrp_party_psup_poststrat(input$epred,
+                             superior_name = "1",
+                             inferior_name = "0",
+                             equal_name = "0.5",
+                             comparison = outcome_name,
+                             subgroups = TRUE,
+                             poststrat_tibble = .poststrat_tibble %>% group_by(partyid),
+                             poststrat_epred = .poststrat_epred)
+
+  partyid_summary$summary <-
+    partyid_summary$summary %>%
+    mutate(new_label = glue::glue("**{nice_num(mean, 2, TRUE)}** [{nice_num(lower, 2, TRUE)}; {nice_num(upper, 2, TRUE)}]"),
+           grouping_type = "Party affiliation") %>%
+    relocate(comparison, grouping_type, new_label)
+
+  ## age_fine
+  print(glue::glue("stratifying by age_fine for {outcome_name}"))
+  age_fine_summary <-
+    mrp_party_psup_poststrat(input$epred,
+                             superior_name = "1",
+                             inferior_name = "0",
+                             equal_name = "0.5",
+                             comparison = outcome_name,
+                             subgroups = TRUE,
+                             poststrat_tibble = .poststrat_tibble %>% group_by(age_fine),
+                             poststrat_epred = .poststrat_epred)
+
+  age_fine_summary$summary <-
+    age_fine_summary$summary %>%
+    mutate(new_label = glue::glue("**{nice_num(mean, 2, TRUE)}** [{nice_num(lower, 2, TRUE)}; {nice_num(upper, 2, TRUE)}]"),
+           grouping_type = "Age") %>%
+    relocate(comparison, grouping_type, new_label)
+
+  ## state
+  print(glue::glue("stratifying by state for {outcome_name}"))
+  state_summary <-
+    mrp_party_psup_poststrat(input$epred,
+                             superior_name = "1",
+                             inferior_name = "0",
+                             equal_name = "0.5",
+                             comparison = outcome_name,
+                             subgroups = TRUE,
+                             poststrat_tibble = .poststrat_tibble %>% group_by(state),
+                             poststrat_epred = .poststrat_epred)
+
+  state_summary$summary <-
+    state_summary$summary %>%
+    mutate(new_label = glue::glue("**{nice_num(mean, 2, TRUE)}** [{nice_num(lower, 2, TRUE)}; {nice_num(upper, 2, TRUE)}]"),
+           grouping_type = "State") %>%
+    relocate(comparison, grouping_type, new_label)
+
+  ## region
+  print(glue::glue("stratifying by region for {outcome_name}"))
+  region_summary <-
+    mrp_party_psup_poststrat(input$epred,
+                             superior_name = "1",
+                             inferior_name = "0",
+                             equal_name = "0.5",
+                             comparison = outcome_name,
+                             subgroups = TRUE,
+                             poststrat_tibble = .poststrat_tibble %>% group_by(region),
+                             poststrat_epred = .poststrat_epred)
+
+  region_summary$summary <-
+    region_summary$summary %>%
+    mutate(new_label = glue::glue("**{nice_num(mean, 2, TRUE)}** [{nice_num(lower, 2, TRUE)}; {nice_num(upper, 2, TRUE)}]"),
+           grouping_type = "Region") %>%
+    relocate(comparison, grouping_type, new_label)
+
+  output <-
+    list("population" = population_summary,
+         "education_collapse" = education_collapse_summary,
+         "age_fine" = age_fine_summary,
+         "race" = race_summary,
+         "income_ces" = income_ces_summary,
+         "partyid" = partyid_summary,
+         "male" = male_summary,
+         "region" = region_summary,
+         "state" = state_summary)
+
+  if(save_output == TRUE) {
+    saveRDS(output,
+            glue::glue("mrp_poststrats/{outcome_name}_poststrat.rds"))
+  }
+
+  if(return_state == FALSE) {
+    output <-
+      list("population" = population_summary,
+           "education_collapse" = education_collapse_summary,
+           "age_fine" = age_fine_summary,
+           "race" = race_summary,
+           "income_ces" = income_ces_summary,
+           "partyid" = partyid_summary,
+           "male" = male_summary,
+           "region" = region_summary)
+  }
+
+  return(output)
+
+}
---
+++ R/psup_mrp.R
@@ -0,0 +1,138 @@
+#' Run PSup MRP
+#'
+#' Perform the regression and epred steps of a multilevel regression and poststratification (MRP) for a partially supervised (PSup) categorical outcome variable.
+#'
+#' @param variable_name String. The name of the categorical PSup variable to be modeled.
+#' @param variable_label Optional. A human-readable label for the variable. Defaults to \code{variable_name}.
+#' @param my_data A data frame containing the data for the regression model. Defaults to \code{set_my_data}.
+#' @param save_model Logical. If \code{TRUE}, saves the fitted model object to an mrp_models folder in your directory. Defaults to \code{save_my_model}.
+#' @param save_epred Logical. If \code{TRUE}, saves the posterior epred draws to an mrp_epreds folder in your directory. Defaults to \code{save_my_epred}.
+#' @param my_prior A list of prior distributions for the model. Defaults to:
+#'   \itemize{
+#'     \item \code{Intercept: normal(0, 1.25)}
+#'     \item \code{Intercept (mu0, mu1): normal(0, 1)}
+#'     \item \code{Coefficients: normal(0, 1)}
+#'     \item \code{Random effects (mu0, mu1): exponential(2)}
+#'   }
+#' @param my_init Initial values for the model. Default is \code{0}.
+#' @param my_iter Total number of iterations per chain. Default is \code{set_my_iter}.
+#' @param my_warmup Number of warmup iterations for the sampler. Default is \code{set_my_warmup}.
+#' @param my_refresh Frequency of progress updates during model fitting. Default is \code{250}.
+#' @param my_poststrat A data frame for poststratification. Default is \code{set_my_poststrat}.
+#' @param my_adapt_delta Numeric value for the Stan sampler control parameter to improve convergence. Default is \code{set_my_adapt_delta}.
+#'
+#' @return A list containing:
+#'   \itemize{
+#'     \item \code{"model"}: The fitted \code{brms} model object.
+#'     \item \code{"epred"}: Posterior predictions from the fitted model.
+#'   }
+#'
+#' @details
+#' This function performs multilevel Bayesian regression using the \code{brms} package.
+#' It constructs a model formula based on a predefined structure, fits the model
+#' using the \code{cmdstanr} backend, and generates posterior epred draws for
+#' a target population. The model and predictions can be optionally saved for future use.
+#'
+#' If the variable is numeric (0, 0.5, 1), it will be converted to a factor with 0.5 as the reference category.
+#'
+#' @note Requires the \code{brms} and \code{cmdstanr} packages.
+#' Ensure that the \code{my_data} and \code{my_poststrat} arguments are properly formatted.
+#'
+#' @export
+psup_mrp <- function(variable_name,
+                     variable_label = NULL,
+                     my_data = set_my_data,
+                     save_model = save_my_model,
+                     save_epred = save_my_epred,
+                     my_prior = c(brms::set_prior("normal(0 , 1.25)", class = "Intercept"),
+                                  brms::set_prior("normal(0 , 1)", class = "Intercept", dpar = "mu0"),
+                                  brms::set_prior("normal(0 , 1)", class = "Intercept", dpar = "mu1"),
+                                  brms::set_prior("normal(0 , 1)", class = "b"),
+                                  brms::set_prior("exponential(2)", class = "sd", dpar = "mu0"),
+                                  brms::set_prior("exponential(2)", class = "sd", dpar = "mu1")),
+                     my_init = 0,
+                     my_iter = set_my_iter,
+                     my_warmup = set_my_warmup,
+                     my_refresh = 250,
+                     my_poststrat = set_my_poststrat,
+                     my_adapt_delta = set_my_adapt_delta) {
+
+  print("Numeric PSup outcome (0, 0.5, 1) will be converted to a factor, with 0.5 as the reference category")
+  my_data <-
+    my_data %>%
+    mutate(!!sym(variable_name) := factor(!!sym(variable_name),
+                                          levels = c("0.5", "0", "1")))
+
+  if(is.null(variable_label)) {
+    variable_label <- variable_name
+  }
+
+  variable_name <-
+    str_replace_all(variable_name,
+                    " - | |-",
+                    "_")
+
+  names(my_data) <-
+    str_replace_all(names(my_data),
+                    " - | |-",
+                    "_")
+
+  formula_string <-
+    " ~ (1 | state) + (1 | race) + (1 | age_fine) +
+  (1 | education_collapse) + (1 | income_ces) + male + (1 | male:race) +
+  (1 | education_collapse:age_fine) + (1 | partyid) + (1 | region) + repvote_sd_cent"
+
+  psup_form <-
+    as.formula(glue::glue("`{variable_name}`{formula_string}"))
+
+  print(glue::glue("Running regression for {variable_label}"))
+
+  psup_fit <-
+    brm(formula = psup_form,
+        family = categorical(),
+        data = my_data,
+        control = list(adapt_delta = my_adapt_delta, max_treedepth = 15),
+        prior = my_prior,
+        chains = 4,
+        cores = 4,
+        iter = my_iter,
+        warmup = my_warmup,
+        init = my_init,
+        backend = 'cmdstanr',
+        threads = threading(4),
+        seed = 1010,
+        silent = 0,
+        refresh = my_refresh,
+        stan_model_args = list(stanc_options = list('O1')))
+
+  print(glue::glue("Completed regression for {variable_label}"))
+
+  print(glue::glue("Computing posterior predictions for {variable_label}"))
+
+  psup_epred <-
+    posterior_epred(object = psup_fit,
+                    newdata = my_poststrat,
+                    ndraws = 1000,
+                    allow_new_levels = TRUE)
+
+  print(glue::glue("Completed posterior predictions for {variable_label}"))
+
+
+  if(save_epred == TRUE) {
+    print(glue::glue("Saving posterior predictions for {variable_label}"))
+
+    saveRDS(psup_epred,
+            file = glue::glue("mrp_epreds/mrp_epred_{variable_name}.rds"))
+  }
+
+  if(save_model == TRUE) {
+    print(glue::glue("Saving regression model for {variable_label}"))
+
+    save(psup_fit,
+         file = glue::glue("mrp_models/mrp_model_{variable_name}.RData"))
+  }
+
+  return(list("model" = psup_fit,
+              "epred" = psup_epred))
+
+}
