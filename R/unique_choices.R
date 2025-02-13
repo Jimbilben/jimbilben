10421f67ad75f7b163d285f61ad3d95d1561c0ae
Jimbilben <jamie@rethinkpriorities.org>
2025-02-13 14:51:50 +0000

    Updating unique_choices
    Adds backslashes to parentheses for better detection of strings within text

--- R/unique_choices.R
+++ R/unique_choices.R
@@ -4,8 +4,9 @@
 #' @param data the target dataframe
 #' @param variable the variable/column name in which the multiple choice items are stored
 #' @param prefix = A string. Each variable name should, but does not have to be, preceded by a prefix so that the new variables can be easily identified/selected and e.g., pivot_longer from. Defaults to choice_
+#' @param add_backslash Logical, defaults to TRUE. If TRUE, parentheses will be preceded by two backslashes e.g., \\(this\\)
 
-unique_choices <- function(data, variable, sep = ",") {
+unique_choices <- function(data, variable, sep = ",", add_backslash = TRUE) {
 
   variable_quoted <- rlang::enquo(variable)
 
@@ -13,6 +14,9 @@ unique_choices <- function(data, variable, sep = ",") {
 
   unique_choices <- unique(unlist(stringr::str_split(na.omit(target_column), sep)))
 
-  return(unique_choices)
+  if (add_backslash) {
+    unique_choices <- stringr::str_replace_all(unique_choices, "([()])", "\\\\\\1")
+  }
 
+  return(unique_choices)
 }
