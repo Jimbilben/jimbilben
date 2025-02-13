bfaf4c200612bb5c20b4202bc96ab366254b2081
Jimbilben <jamie@rethinkpriorities.org>
2025-02-13 14:05:47 +0000

    update mrp formula print out

--- R/code_mrp_form.R
+++ R/code_mrp_form.R
@@ -9,4 +9,8 @@ code_mrp_form <- function() {
               (1 | education_collapse) + (1 | income_ces) + male + (1 | male:race) +
               (1 | education_collapse:age_fine) + (1 | partyid) + (1 | region) + z_repvote")
 
+  glue::glue("~ (1 | state) + (1 | race) + (1 | age_fine) +
+              (1 | education_collapse) + (1 | income_ces) + male + (1 | male:race) +
+              (1 | education_collapse:age_fine) + (1 | partyid) + (1 | region) + repvote_cent")
+
 }
