b2e1eecd437d5a94fdff395dc0c25596f09ec614
Jimbilben <jamie@rethinkpriorities.org>
2025-07-24 17:34:39 +0100

    Update binary_comparison.R

--- R/binary_comparison.R
+++ R/binary_comparison.R
@@ -27,3 +27,60 @@
 #'   previous proportions, and the log of that ratio. These values are added to the
 #'   `current` list under the `difference` sublist, using the provided `name_previous`
 #'   string to label the comparison.
+binary_comparison <- function(current,
+                              previous,
+                              name_previous = "2024",
+                              .decimals = 2) {
+
+  my_diff_string <-
+    glue::glue("difference_{name_previous}")
+
+  difference_tib <-
+    current$population$posterior %>%
+    dplyr::mutate(temp_proportion = previous$population$posterior$proportion,
+                  temp_diff = proportion - temp_proportion,
+                  temp_prop = proportion / temp_proportion,
+                  temp_logprop = log(temp_prop))
+
+  difference_tib_summary <-
+    difference_tib %>%
+    dplyr::mutate(temp_diff = temp_diff * 100) %>%
+    tidyr::pivot_longer(
+      cols = c(temp_diff,
+               temp_prop,
+               temp_logprop),
+      names_to = "type",
+      values_to = "value"
+    ) %>%
+    dplyr::group_by(
+      type
+    ) %>%
+    nice_post(
+      estimate = value,
+      decimals = .decimals,
+      remove_lead = FALSE
+    )
+
+  names(difference_tib)[names(difference_tib) == "temp_proportion"] <- glue::glue("proportion_{name_previous}")
+  names(difference_tib)[names(difference_tib) == "temp_diff"] <- glue::glue("diff_{name_previous}")
+  names(difference_tib)[names(difference_tib) == "temp_prop"] <- glue::glue("prop_{name_previous}")
+  names(difference_tib)[names(difference_tib) == "temp_logprop"] <- glue::glue("logprop_{name_previous}")
+
+  difference_tib_summary <-
+    difference_tib_summary %>%
+    dplyr::mutate(
+      type = dplyr::case_when(type == "temp_diff" ~ glue::glue("diff_{name_previous}"),
+                              type == "temp_prop" ~ glue::glue("prop_{name_previous}"),
+                              type == "temp_logprop" ~ glue::glue("logprop_{name_previous}"))
+    )
+
+
+  current[[my_diff_string]]$posterior <-
+    difference_tib
+
+  current[[my_diff_string]]$summary <-
+    difference_tib_summary
+
+  return(current)
+
+}
